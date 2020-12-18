7ce5ab353637b49b6d3a9be4e258082ce58a0ee1
aohuidai <50763837+aohuidai@users.noreply.github.com>
2020-12-19 01:36:14 +0800

    Add files via upload

---
+++ chatroom/src/Server/Server.java
@@ -0,0 +1,53 @@
+package Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public  int  numberOfGuest=0;
+
+
+    public static void main(String arg[]) {
+        ServerSocket serverSocket = null;
+        Socket sc=null;
+        try {
+            serverSocket = new ServerSocket(23333);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        while (true)
+        {
+
+            try {
+
+                System.out.println("等待ing");
+                sc = serverSocket.accept();
+
+                AWaiter re =new AWaiter(sc);
+                System.out.println("有人进来了");
+
+
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
